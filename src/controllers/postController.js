5cb5fe8482c8a3a729569419a10887b33c8912fa
Nitin Khare <kharenitin777@gmail.com>
2024-10-11 09:05:22 +0530

    added middlware demo

---
+++ src/controllers/postController.js
@@ -0,0 +1,4 @@
+export async function createPost(req, res) {
+    //call the service layer
+    return res.json({message: 'Post created successfully'})
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import express from "express";
 import connectDB from "./config/dbConfig.js";
-
-const app = express();
+import { createPost } from "./controllers/postController.js";
 
 const PORT = 3000;
+
+const app = express(); 
+
+app.use(express.json()); //middleware to parse json data(serializer, deserializer)
+
+app.use(express.text());  //deserialization of text, json, urlencoded
+
+app.use(express.urlencoded());
+
 
 app.get('/', (req,res) => {
     return res.send('Home');
 });
 
 app.get('/ping', (req, res) => {
+    // const name = req.params.name; //req params -> { name:'value', id:2 }
+    console.log(req.query);
+    console.log(req.body);
     return res.json({"message": "pong"});
-})
+});
+
+// function m1(req, res, next){
+//     console.log('M1');
+//     next();
+// }
+
+// function m2(req, res, next){
+//     console.log('M2');
+//     next();
+// }
+
+// function m3(req, res, next){
+//     console.log('M3');
+//     next();
+// }
+
+app.post('/posts', createPost);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT}`);
